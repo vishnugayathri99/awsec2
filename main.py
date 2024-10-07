8f54e4d43774076c6d50119a398b87ee9dbcc0c1
vishnugayathri99 <77778193+vishnugayathri99@users.noreply.github.com>
2024-10-07 19:24:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ app = FastAPI()
 @app.get("/")
 def Hello():
     return ("Hello world")
+
+if __name__ == "__main__":
+    uvicorn.run("__main__:app", host="0.0.0.0", port=8000, reload=True, workers=2)
